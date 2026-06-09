9c02c54f035305e8ccdbcae43a12738544b385c8
HoussamLh <lahlahhoussam80@gmail.com>
2026-06-09 23:40:40 +0100

    integrate subscription system component to the landing page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Hero} from '../features/home/Hero';
 import { TrustSection } from '../features/home/Trust-section';
+import {SubscriptionSection} from '../features/home/Subscription'
 
 export const Home: React.FC = () => {
 return(
   <div style={{padding:'0 8%',overflow:'hidden'}}>
     <Hero/>
     <TrustSection/>
+    <SubscriptionSection/>
   </div>
 )
 };  
