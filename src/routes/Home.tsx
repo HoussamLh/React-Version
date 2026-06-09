afaf1d62074573a77c414b2c43d1dc988fce0a20
HoussamLh <lahlahhoussam80@gmail.com>
2026-06-09 22:13:22 +0100

    integrate trust section into home view with mobile responsive design

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Hero} from '../features/home/Hero';
+import { TrustSection } from '../features/home/Trust-section';
 
 export const Home: React.FC = () => {
 return(
   <div style={{padding:'0 8%',overflow:'hidden'}}>
     <Hero/>
+    <TrustSection/>
   </div>
 )
-};
+};  
