eb330a4bcad3f5776498fe1e793f54c6397736e3
HoussamLh <lahlahhoussam80@gmail.com>
2026-06-10 13:50:16 +0100

    mount core domains component into about sequence row

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Hero } from "../features/about/Hero";
+import { CoreDomains } from "../features/about/CoreDomains";
 export const About: React.FC = () => (
-  <div style={{padding:'2%', overflow:'hidden'}}>
+  <div style={{padding:'0 8%', overflow:'hidden'}}>
     <Hero />
+    <CoreDomains />
   </div>
 );
 
