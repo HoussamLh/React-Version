f024acdd21fc9e4a720effc8d197c36b31de25c7
HoussamLh <lahlahhoussam80@gmail.com>
2026-06-14 17:13:53 +0100

    Normalize component naming conventions

--- src/features/services/index.ts
+++ src/features/services/index.ts
@@ -1,4 +1,4 @@
-export { Hero } from "./sections/Hero";
+export { Hero } from "./sections/HeroSection";
 export { ServicesSection } from "./sections/ServicesSection";
 export { WorkflowSection } from "./sections/WorkflowSection";
 export { CTASection } from "./sections/CTASection";
