dc70770fd2885b360d405210afb1c194697bd35a
HoussamLh <lahlahhoussam80@gmail.com>
2026-06-14 17:10:57 +0100

    Add public export index for feature module

---
+++ src/features/services/index.ts
@@ -0,0 +1,4 @@
+export { Hero } from "./sections/Hero";
+export { ServicesSection } from "./sections/ServicesSection";
+export { WorkflowSection } from "./sections/WorkflowSection";
+export { CTASection } from "./sections/CTASection";
