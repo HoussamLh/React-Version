8789f4f54db7a65c3ce83a584dd6c0a2b86a9013
HoussamLh <lahlahhoussam80@gmail.com>
2026-06-18 14:28:13 +0100

    clean home feature exports

---
+++ src/features/home/sections/index.ts
@@ -0,0 +1,4 @@
+export { Hero } from "./Hero";
+export { TrustSection } from "./Trust-section";
+export { SubscriptionSection } from "./Subscription";
+export { CTASection } from "./CTASection";
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import {Hero} from '../features/home/Hero';
-import { TrustSection } from '../features/home/Trust-section';
-import {SubscriptionSection} from '../features/home/Subscription';
-import { CTASection } from "../features/home/CTASection";
+import {
+  CTASection,
+  Hero,
+  SubscriptionSection,
+  TrustSection,
+} from "../features/home/sections";
 
 export const Home: React.FC = () => {
-return(
-  <div style={{padding:'0 8%',overflow:'hidden'}}>
-    <Hero/>
-    <TrustSection/>
-    <SubscriptionSection/>
-    <CTASection/>
-  </div>
-)
-};  
+  return (
+    <div style={{ padding: "0 8%", overflow: "hidden" }}>
+      <Hero />
+      <TrustSection />
+      <SubscriptionSection />
+      <CTASection />
+    </div>
+  );
+};
