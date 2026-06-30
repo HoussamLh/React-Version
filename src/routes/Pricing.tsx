a53a783079bb6390b1ff5c07a8df9acfad976375
HoussamLh <lahlahhoussam80@gmail.com>
2026-06-30 16:00:42 +0100

    add pricing page route

--- src/routes/Pricing.tsx
+++ src/routes/Pricing.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
-export const Pricing: React.FC = () => (
-  <div style={styles.page}>
-    <h1>Pricing Page</h1>
-  </div>
-);
-const styles = { page: { padding: "100px 8%", color: "#fff" } };
+import React from "react";
+import {
+  CTASection,
+  ComparisonSection,
+  HeroSection,
+  MaintenanceSection,
+  PricingSection,
+} from "../features/pricing";
+
+export const Pricing: React.FC = () => {
+  return (
+    <div className="ds-page">
+      <HeroSection />
+      <PricingSection />
+      <MaintenanceSection />
+      <ComparisonSection />
+      <CTASection />
+    </div>
+  );
+};
