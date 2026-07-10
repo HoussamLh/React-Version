d452c984cb4dc4618c088be78d810bb9c185d639
HoussamLh <lahlahhoussam80@gmail.com>
2026-07-10 12:55:02 +0100

    Create shared AdminSearchInput component

--- src/features/admin/components/index.ts
+++ src/features/admin/components/index.ts
@@ -2,3 +2,4 @@ export { AdminErrorRecovery } from "./AdminErrorRecovery";
 export { AdminActionButton } from "./AdminActionButton";
 export { AdminStatusBadge } from "./AdminStatusBadge";
 export { AdminEmptyState } from "./AdminEmptyState";
+export { AdminSearchInput } from "./AdminSearchInput";
