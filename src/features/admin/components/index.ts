3bcadbd949efcbaa6eab330333444be50a97455d
HoussamLh <lahlahhoussam80@gmail.com>
2026-07-10 13:19:11 +0100

    Create shared AdminPanel component

--- src/features/admin/components/index.ts
+++ src/features/admin/components/index.ts
@@ -3,3 +3,4 @@ export { AdminActionButton } from "./AdminActionButton";
 export { AdminStatusBadge } from "./AdminStatusBadge";
 export { AdminEmptyState } from "./AdminEmptyState";
 export { AdminSearchInput } from "./AdminSearchInput";
+export { AdminPanel } from "./AdminPanel";
