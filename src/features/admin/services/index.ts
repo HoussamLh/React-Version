fe93c6f232f55b96b42f964be12af89b0716017e
HoussamLh <lahlahhoussam80@gmail.com>
2026-07-12 18:26:29 +0100

    feat: add services CMS types and service

---
+++ src/features/admin/services/index.ts
@@ -0,0 +1,15 @@
+export type {
+  AdminService,
+  AdminServiceFormValues,
+  ServiceAccent,
+  ServiceIcon,
+  ServiceSpan,
+  ServiceStatus,
+} from "./servicesCms.types";
+
+export {
+  createAdminService,
+  deleteAdminService,
+  getAdminServices,
+  updateAdminService,
+} from "./servicesCms.service";
