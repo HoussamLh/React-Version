709b686528713c6413d1be0d64af212a14604dfc
HoussamLh <lahlahhoussam80@gmail.com>
2026-07-13 01:57:45 +0100

    refactor: remove old home subscription data

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -1,4 +1,3 @@
 export { HeroTerminalCard } from "./HeroTerminalCard";
 export { TrustFeatureCard } from "./TrustFeatureCard";
 export { TrustStatCard } from "./TrustStatCard";
-export { SubscriptionCard } from "./SubscriptionCard";
