dfdd886fc35ab522d03e12974dd7efc9bc2191ea
HoussamLh <lahlahhoussam80@gmail.com>
2026-08-24 11:24:23 +0100

    refactor: update about static image using cloudinary link

--- src/features/pages/about/sections/Hero.tsx
+++ src/features/pages/about/sections/Hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import heroImage from "../../../../Assets/heroAbout.png";
 import { SharedHero } from "../../../../design-system";
+
+const heroAbout =
+  "https://res.cloudinary.com/pekcmwlo/image/upload/v1787091789/heroAbout.jpg";
 
 export const Hero: React.FC = () => {
   return (
@@ -9,7 +11,7 @@ export const Hero: React.FC = () => {
       titleStart="Architecting the Future of"
       titleAccent="Digital Commerce."
       subtitle="Devbysam, we engineer scalable, resilient systems that empower enterprise stakeholders and forward-thinking founders to lead their industries with confidence."
-      image={heroImage}
+      image={heroAbout}
       imageAlt="Data Center Server Infrastructure"
       hoverAccent="blue"
     />
